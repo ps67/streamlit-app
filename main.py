8e3ca00270460b3ee4c7c181f5b6e97d7ce5e381
ps67 <psaucy-67@yahoo.fr>
2025-11-08 20:47:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 #import plotly.express as px
 import json
 import os
+import urllib.request
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💰", layout="wide")
 
@@ -143,6 +144,7 @@ def main():
         
 
 main()
+
 
 
 

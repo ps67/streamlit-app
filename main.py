c3efe089cd08ac89bfdf80c416f926f93a806e69
ps67 <psaucy-67@yahoo.fr>
2025-11-08 18:14:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💰", layout="wide")
 
-category_file = "/categories.json"
+category_file = "./categories.json"
 
 if "categories" not in st.session_state:
     st.session_state.categories = {
@@ -139,5 +139,6 @@ def main():
         
 
 main()
+
 
 
